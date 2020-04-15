264bd00fb523a9072a49eb8fe0ced76bd86f4a32
megha <megha.magento@gmail.com>
2020-04-15 10:04:23 +0530

    Get product stock qty and stock information by product id

--- app/code/Training/Product/Block/Index/Index.php
+++ app/code/Training/Product/Block/Index/Index.php
@@ -5,16 +5,17 @@ namespace Training\Product\Block\Index;
 
 class Index extends \Magento\Framework\View\Element\Template {
 
-    public function __construct(\Magento\Catalog\Block\Product\Context $context, array $data = []) {
-
+    public function __construct(\Magento\Catalog\Block\Product\Context $context, \Magento\CatalogInventory\Api\StockRegistryInterface $stockItemRepository, array $data = []) {
+        $this->stockItemRepository = $stockItemRepository;
         parent::__construct($context, $data);
-
     }
-
-
     protected function _prepareLayout()
     {
         return parent::_prepareLayout();
+    }
+     public function getStockItem($productId)
+    {
+        return $this->stockItemRepository->getStockItem($productId);
     }
 
 }
