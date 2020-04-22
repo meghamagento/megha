9759960fb62d96cdba7d84009a8ffb539895693c
megha <megha.magento@gmail.com>
2020-04-22 13:45:51 +0530

    check customer is authenticated

--- app/code/Training/Customer/Block/Index/Index.php
+++ app/code/Training/Customer/Block/Index/Index.php
@@ -5,8 +5,9 @@ namespace Training\Customer\Block\Index;
 
 class Index extends \Magento\Framework\View\Element\Template {
 
-    public function __construct(\Magento\Catalog\Block\Product\Context $context,\Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,array $data = []) {
+    public function __construct(\Magento\Catalog\Block\Product\Context $context,\Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,\Magento\Customer\Model\Session\Proxy $customer,array $data = []) {
         $this->customerRepository = $customerRepository;
+        $this->customer = $customer;
         parent::__construct($context, $data);
 
     }
@@ -20,6 +21,16 @@ class Index extends \Magento\Framework\View\Element\Template {
      public function getCustomerByEmail($email)
     {
         return $this->customerRepository->get($email);
+    }
+    // check customer is authenticated
+    public function getCustomerLogIn(){
+        if ($this->customer->getCustomer()->getGroupId() != \Magento\Customer\Model\GroupManagement::NOT_LOGGED_IN_ID) {
+            $result = "logged in" ;
+        }
+        else{
+            $result = "not logged in";
+        }
+        return $result;
     }
 
 }
